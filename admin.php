6c405715b39e8d1d2d5fe1a098811710b537fc23
Eddo Dava <eddodava@gmail.com>
2023-08-14 14:58:34 +0700

    membuat navbar

--- admin.php
+++ admin.php
@@ -14,24 +14,52 @@ if (!isset($_SESSION["username"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Admin Dashboard - Instansi XYZ</title>
     <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" href="plugin/css/bootstrap.min.css">
+    <style>
+        /* Custom styles for the header */
+        .custom-header {
+            background-color: #306ee8;
+            color: white;
+            padding: 20px 0;
+        }
+
+        /* Custom styles for the navbar */
+        .custom-navbar {
+            background-color: #306ee8;
+        }
+
+        /* Custom styles for the content */
+        .custom-content {
+            background-color: #F2F2F2;
+            padding: 20px;
+        }
+    </style>
 </head>
 <body>
-    <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Admin Dashboard</div>
-                    <div class="card-body">
-                        <h2>Welcome, <?php echo $_SESSION["username"]; ?></h2>
-                        <!-- Add your admin dashboard content here -->
-                        <a href="logout.php" class="btn btn-danger">Logout</a>
-                    </div>
-                </div>
+    <nav class="navbar navbar-expand-lg navbar-dark custom-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/logo-BPSDMP.png" alt="Bpsdmp Surabaya Logo" width="50" class="mr-2">
+                Bpsdmp Surabaya
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+
+    <div class="custom-content container mt-5">
+        <h2>Welcome, <?php echo $_SESSION["username"]; ?></h2>
+        <!-- Add admin dashboard content here for managing activities -->
     </div>
 
     <!-- Include Bootstrap JS -->
--- index.php
+++ index.php
@@ -3,13 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>Instansi XYZ</title>
     <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" href="plugin/css/bootstrap.min.css">
+    <style>
+        /* Custom styles for the header */
+        .custom-header {
+            background-color: #2D3876;
+            color: white;
+            padding: 20px 0;
+        }
+
+        /* Custom styles for the navbar */
+        .custom-navbar {
+            background-color: #306ee8;
+        }
+
+        /* Custom styles for the content */
+        .custom-content {
+            background-color: #F2F2F2;
+            padding: 20px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Welcome to the Homepage</h1>
-    <!--  homepage content here -->
+    <nav class="navbar navbar-expand-lg navbar-dark custom-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/logo-BPSDMP.png" alt="Bpsdmp Surabaya Logo" width="50" class="mr-2">
+                Bpsdmp Surabaya
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="custom-content container mt-5">
+        <h2 class="mb-4">Documentations of Recent Activities</h2>
+        <!-- Add dynamic content here from the database -->
+    </div>
 
     <!-- Include Bootstrap JS -->
     <script src="plugin/js/bootstrap.min.js"></script>
